388d980a9b61b389a0691983b648608f15baa41d
nlnamanh2k2 <namanh2k2av@gmail.com>
2022-10-14 10:47:15 +0700

    commit

--- Buoi1/Bai1.cpp
+++ Buoi1/Bai1.cpp
@@ -2,6 +2,7 @@
 
 using namespace std;
 
+// Cach 1
 void output(int a[], int n){
 	for(int i=1;i<=n;i++)
 		cout<<a[i];
@@ -34,3 +35,34 @@ int main(){
 	cin>>n;
 	sinh_nhi_phan(n);
 }
+
+//Cach 2
+/*
+int *a;
+int n;
+
+void show(int a[]){
+	for(int i=1;i<=n;i++)
+		cout<<a[i];
+	cout<<endl;
+}
+
+void Try(int k){
+	for (int i=0;i<=1;i++){
+		a[k] = i;
+		if(k == n){
+			show(a);
+		} else {
+			Try(k+1);
+		}
+	}
+}
+
+int main(){
+	cin>>n;
+	a = new int[n+1];
+	for(int i=0;i<=n;i++)
+		a[i] = 0;
+	Try(1);
+}
+*/
